4118328e939ca45302a7042eab94fd9419ff0a05
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-02-26 03:10:20 +0000

    eight

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
+
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://dog.ceo/api/breeds/image/random')
+  alert("run App.jsx")
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header><p>Amazing Himoooooono</p></header>
+      <main>
+        <p>ひもののサイトです</p>
+        <img src={dogUrl}></img>
+        <button onClick={setDogUrl(fetch('https://dog.ceo/api/breeds/image/random'))}>更新</button>
+      </main>
     </div>
   )
 }
