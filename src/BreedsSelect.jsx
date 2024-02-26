e3af985b471f9f0cb98a9f9527ac7fcd54311352
ひもの <128397176+Himoooooono@users.noreply.github.com>
2024-02-26 06:06:58 +0000

    Add DogListContainer component and update BreedsSelect component

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,12 @@
-// @ts-check
+/* eslint-disable react/prop-types */
+// @ts-nocheck
 
-export const BreedsSelect = () => {
-  return <></>
+export const BreedsSelect = ({breeds, selectedBreed, onChange}) => {
+  const dogs=Object.keys(breeds).map((key)=>
+    <option value={key} key={key}>{key}</option>
+  );
+  alert(dogs);
+  return <select value={selectedBreed} onChange={onChange}>{dogs}</select>
 }
 
 export default BreedsSelect
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,27 @@
 // @ts-check
 
+import { useEffect, useState } from "react"
+import BreedsSelect from "./BreedsSelect"
+
 export const DogListContainer = () => {
-  return <></>
+  const [breeds, setBreeds] = useState()
+  const [selectedBreed, setSelectedBreed] = useState()
+
+  // @ts-ignore
+  const handleSelectChange = (e) => {
+    setSelectedBreed(e.target.value)
+  }
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setBreeds(data.message)
+      })
+  })
+
+  // @ts-ignore
+  return <BreedsSelect breeds={breeds} selectedBreed={selectedBreed} onChange={handleSelectChange}/>
 }
 
 export default DogListContainer
